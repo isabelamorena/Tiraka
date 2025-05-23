ec1833752d3bda2dc8f0b69b71bb6b3e1b640a59
Isabel Yangxin Moreno Basurto <isabelm.basurto@gmail.com>
2025-05-23 22:29:00 +0200

    bcryptjs

--- back/routes/fencer-controlPanel.js
+++ back/routes/fencer-controlPanel.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 /* ------------------------------------------ Asistencias ---------------------------------------------------------------- */
 // Añadir un registro de asistencia
