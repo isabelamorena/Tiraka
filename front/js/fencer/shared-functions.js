69db0fbc3265ddfaa85aec224ba34bc61f71aa57
Isabel Yangxin Moreno Basurto <isabelm.basurto@gmail.com>
2025-06-03 10:50:48 +0200

    shared-functions

--- front/js/fencer/shared-functions.js
+++ front/js/fencer/shared-functions.js
@@ -1,21 +1,23 @@
-function showPanel(panelId) {
-    const panels = [
-        "profile",
-        "diary-class",
-        "competition-diary",
-        "create-workout",
-        "training-templates",
-        "attendance-record",
-        "main-content"
-    ];
-    panels.forEach(id => {
-        const el = document.getElementById(id);
-        if (el) {
-            if (id === panelId) {
-                el.classList.remove("d-none");
-            } else {
-                el.classList.add("d-none");
+document.addEventListener("DOMContentLoaded", function() {
+   function showPanel(panelId) {
+        const panels = [
+            "profile",
+            "diary-class",
+            "competition-diary",
+            "create-workout",
+            "training-templates",
+            "attendance-record",
+            "main-content"
+        ];
+        panels.forEach(id => {
+            const el = document.getElementById(id);
+            if (el) {
+                if (id === panelId) {
+                    el.classList.remove("d-none");
+                } else {
+                    el.classList.add("d-none");
+                }
             }
-        }
-    });
+        });
 }
+});
